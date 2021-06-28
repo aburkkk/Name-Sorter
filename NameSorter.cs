8ce91a58f08ca50c8a6b0a4dae093755b983cee6
aburkkk <l1411042@std.yildiz.edu.tr>
2021-06-28 14:03:07 +1000

    additional arguments are added

--- NameSorter.cs
+++ NameSorter.cs
@@ -5,16 +5,40 @@ namespace NameSorter
     {
         static void Main(string[] args)
         {
+            
             // Ensure exactly one argument is given
-            if (args.Length != 1)
+           
+           if (args.Length == 2 && args[1] == "0")
+            {
+
+                var filename = args[0];
+                var unsorted = Importer.importNames(filename);
+                var sorted = Sorter.sort(unsorted);
+                Exporter.exportNames(sorted);
+
+            }
+            else if (args.Length == 2 && args[1] == "1")
+            {
+               
+
+                var filename_1 = args[0];
+                var unsorted_1 = Importer.importNames(filename_1);
+                var sorted_1 = ReverseSorter.sort(unsorted_1);
+                Exporter.exportNames(sorted_1);
+            }
+            else
+            {
+                var filename = args[0];
+                var unsorted = Importer.importNames(filename);
+                var sorted = Sorter.sort(unsorted);
+                Exporter.exportNames(sorted);
+            }
+            if (args.Length > 2 || args.Length <1)
             {
                 Console.Error.WriteLine("Incorrect number of arguments entered.");
                 return;
             }
-            var filename = args[0];
-            var unsorted = Importer.importNames(filename);
-            var sorted = Sorter.sort(unsorted);
-            Exporter.exportNames(sorted);
+
         }
     }
 }
---
+++ ReverseSorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NameSorter
+{
+    public class ReverseSorter
+    {
+        public static List<Person> sort(List<Person> unsorted)
+        {
+            //return unsorted.OrderBy(person => person.lastName)
+            //                    .ThenBy(person => person.firstNames)
+            //                    .ToList();
+            
+            return unsorted.OrderByDescending(person => person.lastName).ThenByDescending(person => person.firstNames).ToList();
+            //here laste name and surname sorted reversly
+
+
+        }
+    }
+}
